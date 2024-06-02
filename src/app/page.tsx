16426ffe9f92cc002e8c88ecf9f145b9e753a2a4
Burmikk <byrmik@gmail.com>
2024-06-02 13:02:17 +0200

    tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import StatusLabel, { Status } from './components/status-lable';
+
 export default function Home() {
-    return (
-        <main>
-            <h1>Home Page</h1>
-        </main>
-    );
+  return (
+    <main>
+      <h1>Home Page</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotAcive}>NotAcive</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+    </main>
+  );
 }
