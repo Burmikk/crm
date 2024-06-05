57e84a89acff1ed09dbba03df262e39e4d7aba15
Burmikk <byrmik@gmail.com>
2024-06-05 15:12:45 +0200

    add fonts

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,15 @@
 import { FC } from 'react';
+import Link from 'next/link';
 interface DashboardProps {}
 const Dashboard: FC<DashboardProps> = () => {
-  return <span>Dashbord</span>;
+  return (
+    <div className=" p-10">
+      <h2 className="  text-4xl mb-5">Dashbord</h2>
+      <hr className=" mb-2 font-medium" />
+      <Link className=" text-xl" href={'dashboard/mycompany'}>
+        Companys
+      </Link>
+    </div>
+  );
 };
 export default Dashboard;
